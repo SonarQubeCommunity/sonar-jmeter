a6fda5ef07de0da49973a8a4013b9abe6cdb6a2d
Antonio Calero <acalero@excentia.es>
2011-01-26 16:21:50 +0000

    License headers

--- sonar-jmeter-plugin/src/main/java/es/excentia/sonar/jmeter/exception/JMeterPluginException.java
+++ sonar-jmeter-plugin/src/main/java/es/excentia/sonar/jmeter/exception/JMeterPluginException.java
@@ -1,22 +1,23 @@
 /*
- * SONAR JMETER PLUGIN
+ * Sonar JMeter Plugin
  * Copyright (C) 2010 eXcentia
- * mailto:info AT excentia DOT es
+ * dev@sonar.codehaus.org
  *
- * SONAR JMeter Plugin is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * SONAR JMeter Plugin is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Sonar; if not, write to the Free Software
+ * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
+
 package es.excentia.sonar.jmeter.exception;
 
 public class JMeterPluginException extends RuntimeException {
