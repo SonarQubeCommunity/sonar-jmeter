4725179676d41cfe5aab18e62f156273a7203465
Carlos Fillol Sendra <carfilse@gmail.com>
2011-07-14 08:26:04 +0000

    ver. 0.2

--- jmeter-report-server/dist/jmeter-report-server.sh
+++ jmeter-report-server/dist/jmeter-report-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=`dirname $0`
-JAR=jmeter-report-server-0.2-SNAPSHOT-jar-with-dependencies.jar
+JAR=jmeter-report-server-0.2-jar-with-dependencies.jar
 CLASSPATH=$BASEDIR/properties:$BASEDIR/lib/$JAR
 
 java -classpath $CLASSPATH es.excentia.jmeter.report.server.JMeterReportServer
